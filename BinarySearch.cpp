171d7bc6e70e4b5c71f18513c8760584475bf639
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-04-17 14:55:17 +0700

    Create Looping with for to tells "data Ke-'

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -17,4 +17,13 @@ void input(){
       cout << "\n[!]Jumlah elemen tidak boleh lebih dari 10,silahkan coba lagi\n";
     }
   }
+
+  cout << "\n=================================\n";
+  cout << "       Masukan Elemen Array        \n";
+  cout << "\n=================================\n";
+
+  for (int i = 0; i < nPanjang; i++){
+    cout << "Data ke-" << (i+1) << "=";
+    cin >> element[i];
+  }
 }
