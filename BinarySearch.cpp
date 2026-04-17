564a7786f2fee8192fbdab6bffd10fa57f05c94d
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-04-17 15:09:09 +0700

    create looping with do while bubblesort

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -27,3 +27,27 @@ void input(){
     cin >> element[i];
   }
 }
+
+void bubblesort(){
+  int pass = 1;
+  do{
+    for (int j = 0;j <= nPanjang - 1 - pass;j++){
+      if (element[j] > element [j+1]){
+        int temp = element[j];
+        element[j] = element[j+1];
+        element[j+1] = temp;
+      }
+    }
+    pass++;
+  }while (pass <= nPanjang - 1);
+}
+
+void display(){
+  cout << "\n===================================\n";
+  cout << " Elemen Array setelah Diurutkan (Asc)\n";
+  cout << "\n===================================\n";
+
+  for (int j = 0; j < nPanjang;j++){
+    
+  }
+}
