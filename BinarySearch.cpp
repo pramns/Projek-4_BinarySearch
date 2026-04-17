54ace4e04967e4a92c9fe8ee51a261f40de943e3
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-04-17 14:41:50 +0700

    create looping with while

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 int element[10];
-int npanjang;
+int nPanjang;
 int x;
+
+void input(){
+  while(true){
+    cout << "Masukan banyaknya elemen pada array(maksimal 10) : ";
+    cin >> nPanjang;
+  }
+}
