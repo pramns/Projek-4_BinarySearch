f40300ffa567f38b6b2479a8f161cb5150d3b3ef
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-04-17 14:47:23 +0700

    follow-throught looping with while

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -9,5 +9,12 @@ void input(){
   while(true){
     cout << "Masukan banyaknya elemen pada array(maksimal 10) : ";
     cin >> nPanjang;
+
+    if (nPanjang <= 10){
+      break;
+    }
+    else{
+      cout << "\n[!]Jumlah elemen tidak boleh lebih dari 10,silahkan coba lagi\n";
+    }
   }
 }
