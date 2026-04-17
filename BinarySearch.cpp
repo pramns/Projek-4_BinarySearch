453a0b0ef643d0ddfbfc0a4fbc5deed5d15f7492
Pramudito Nawa Satria <pramuditonawasatria@gmail.com>
2026-04-17 15:36:09 +0700

    Create binarysearch with do while

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -55,3 +55,41 @@ void display(){
   }
   cout << endl;
 }
+
+void binarysearch(){
+  char ulang;
+  do{
+    cout << "\n=============================\n";
+    cout << "    Pencarian Binary Search    \n";
+    cout << "\n=============================\n";
+
+    cout << "Masukan elemen yang ingin dicari : ";
+    cin >> x;
+
+    int low = 0;
+    int high = nPanjang - 1;
+
+    do{
+      int mid = (low + high)/22;
+      if(element[mid] == x){
+        cout << "\n[/]Elemen " << x << "Ditemukan pada indeks " << mid << "\n";
+        return;
+      }
+
+      if (x < element[mid]){
+        high = mid - 1;
+      }
+
+      if (x > element[mid]){
+        low = mid + 1;
+      }
+    }while (low <= high);
+
+    if (low > high){
+      cout << "\n[X] Elemen " << x << "Tidak ditemukan dalam array.\n";
+    }
+
+    cout <<"\n Ingin mencari lagi?(y/n): ";
+    cin >> ulang;
+  }while (ulang == 'y' || ulang == 'Y');
+}
